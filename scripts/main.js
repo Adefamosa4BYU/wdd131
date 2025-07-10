d38c74073bbb37c63daa4ac91fd0a1ebf9d0118a
adefamosaBYU <fam24002@byui.edu>
2025-07-10 12:14:51 +0100

    WDD 131 Project

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+const hamburger = document.getElementById('hamburger');
+    const navMenu = document.getElementById('navMenu');
+
+    hamburger.addEventListener('click', () => {
+      const isOpen = navMenu.classList.toggle('active');
+      hamburger.setAttribute('aria-expanded', isOpen);
+    });
+
+    document.getElementById("copyright").textContent =
+  `©${new Date().getFullYear()} Rubia M. Francesco @ Madagascar`;
+
+document.getElementById("last-modified").textContent =
+  `Last Updated: ${document.lastModified}`;
+
+
+
